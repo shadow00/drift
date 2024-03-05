450042ca0a3d57f1b32286acbc2faa633c1cbe72
Petru Lupsac <2407802-shadow00@users.noreply.gitlab.com>
2024-03-05 19:58:33 +0100

    Truncate newline character from command printout in python script

--- control.py
+++ control.py
@@ -3,10 +3,10 @@ import time
 
 # List of strings to send
 manual_throttle = [f"t{num}\n" for num in range(1060, 1200, 1)]
-strings_to_send = ["a", *manual_throttle, *manual_throttle[::-1], "s"]
+strings_to_send = ["a\n", *manual_throttle, *manual_throttle[::-1], "s\n"]
 delay = 0.05
 # manual_throttle = [f"t{num}\n" for num in [1100, 1200, 1100, 1200, 1100, 1200, 1100, 1200, 1100]]
-# strings_to_send = ["a", *manual_throttle, "s"]
+# strings_to_send = ["a\n", *manual_throttle, "s\n"]
 # delay = 0.1
 
 # Open the serial port
@@ -16,7 +16,7 @@ ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
 # Write each string in the list to the serial port
 for string in strings_to_send:
-    print(f"Sending: {string}")
+    print(f"Sending: {string.rstrip()}")
     ser.write(string.encode('ascii'))
     time.sleep(delay)
 
