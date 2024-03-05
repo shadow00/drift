492e4d579b4ebc7c973354f7c34f778b83f5b026
Petru Lupsac <2407802-shadow00@users.noreply.gitlab.com>
2024-03-05 18:57:03 +0100

    Add basic python script

---
+++ control.py
@@ -0,0 +1,24 @@
+import serial
+import time
+
+# List of strings to send
+manual_throttle = [f"t{num}\n" for num in range(1060, 1200, 1)]
+strings_to_send = ["a", *manual_throttle, *manual_throttle[::-1], "s"]
+delay = 0.05
+# manual_throttle = [f"t{num}\n" for num in [1100, 1200, 1100, 1200, 1100, 1200, 1100, 1200, 1100]]
+# strings_to_send = ["a", *manual_throttle, "s"]
+# delay = 0.1
+
+# Open the serial port
+# Replace '/dev/ttyACM0' with the actual port name if different
+# Adjust the baud rate (e.g., 9600, 115200) as necessary
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+
+# Write each string in the list to the serial port
+for string in strings_to_send:
+    print(f"Sending: {string}")
+    ser.write(string.encode('ascii'))
+    time.sleep(delay)
+
+# Close the serial port
+ser.close()
