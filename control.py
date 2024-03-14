96f4b85196da78d7ac76159d7564c638259bbdf2
Petru Lupsac <2407802-shadow00@users.noreply.gitlab.com>
2024-03-15 00:38:52 +0100

    Measure time elapsed since we opened the serial connection

--- control.py
+++ control.py
@@ -1,6 +1,12 @@
 import serial
 import time
 
+def since(t0):
+    t = time.time()
+    dt = t - t0
+    ts = f"{dt:.6f}"
+    return ts
+
 # List of strings to send
 manual_throttle = [f"t{num}\n" for num in range(1060, 1200, 1)]
 strings_to_send = ["a\n", *manual_throttle, *manual_throttle[::-1], "s\n"]
@@ -15,10 +21,13 @@ delay = 0.05
 # Adjust the baud rate (e.g., 9600, 115200) as necessary
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
+t0 = time.time()
+
 # Write each string in the list to the serial port
 for string in strings_to_send:
-    print(f"Sending: {string.rstrip()}")
+    print(f"{since(t0)} - Sending: {string.rstrip()}")
     ser.write(string.encode('ascii'))
+    print(f"{since(t0)} - Sent: {string.rstrip()}")
     # WARNING: You MUST read the serial output from the Arduino if you want
     # it to actually execute what you told it to do!
     # Either read the lines from here (don't need to print them out though),
