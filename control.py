a62fa7ef1b553e051d157066f54f27bd5aee1a1d
Petru Lupsac <4933508+shadow00@users.noreply.github.com>
2024-05-23 20:43:55 +0200

    Better delay handling in control.py
    Now the arm delay doesn't stack with the regular instruction delay, and the expected runtime is very close to the real value (but we're still losing a few ms every loop).
    Also added final runtime printout at the end, and some more detailed examples for the control sequences.
    Added comment with the currently used stty settings for the Arduino Due.

--- control.py
+++ control.py
@@ -26,9 +26,13 @@ def since(t0):
 # List of strings to send
 arm_delay = 1
 # manual_throttle = [f"t{num}" for num in range(1100, 1300, 2)]  # Smooth ramp
-manual_throttle = [f"t{num}" for num in range(1100, 1801, 50)]  # Small steps (18)
+manual_throttle = [f"t{num}" for num in range(1100, 2001, 50)]  # Small steps (18)
+# manual_throttle = [f"t{num}" for num in range(1100, 1901, 50)]  # Small steps
+# manual_throttle = [f"t{num}" for num in range(1100, 1301, 50)]  # Small steps
 # strings_to_send = ["a", *manual_throttle, *manual_throttle[::-1], "s"]
-strings_to_send = ["a", f"D{arm_delay}", *manual_throttle, *manual_throttle[::-1], "s"]
+# strings_to_send = ["a", *manual_throttle, "D2", *manual_throttle[::-1], "s"]  # Ramp up and down, with extra 2s delay in the middle
+strings_to_send = ["a", *manual_throttle, *manual_throttle[::-1], "s"]  # Ramp up and down
+# strings_to_send = ["a", *manual_throttle, "t1100", "s"]  # Ramp up, then idle and stop
 # delay = 0.2
 # manual_throttle = [f"t{num}" for num in [1100, 1200, 1100, 1200, 1100, 1200, 1100, 1200, 1100]]
 # # strings_to_send = ["a\n", "e7\n", "a\n", *manual_throttle, "s\n"]
@@ -36,8 +40,9 @@ strings_to_send = ["a", f"D{arm_delay}", *manual_throttle, *manual_throttle[::-1
 # # delay = 0.001
 # delay = 0.01  # Smooth ramp
 delay = 0.5  # Small steps
+# delay = 1  # Small steps
 
-expected_time = sum([int(s[1:]) if s[0]=="D" else delay for s in strings_to_send])
+expected_time = sum([int(s[1:]) if s[0]=="D" else arm_delay if s[0]=="a" else delay for s in strings_to_send])
 
 # Open the serial port
 # Replace '/dev/ttyACM0' with the actual port name if different
@@ -45,6 +50,7 @@ expected_time = sum([int(s[1:]) if s[0]=="D" else delay for s in strings_to_send
 # ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
 ser = serial.Serial('/dev/ttyACM0')
 
+# stty -F /dev/ttyACM0 raw 115200 -iexten -echo -echoe -echok -echoctl -echoke
 # stty -F /dev/ttyACM0 raw -iexten -echo -echoe -echok -echoctl -echoke -onlcr
 # stty -F /dev/ttyACM0 cs8 9600 ignbrk -brkint -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts
 # tail -f /dev/ttyACM0 -s 0.0001 >> test_format5.txt
@@ -82,12 +88,21 @@ for string in strings_to_send:
     #     print(f"{since(t0)} -", l.decode('ascii').rstrip())
     # resp = ser.read_until('\n')
     # Sending messages and reading the response takes some time, it makes the timing less accurate
-    now = time.time()
-    if delay - (now - start) > 0:
-        time.sleep(delay - (now - start))
+    if string[0] == "a":  # If we just sent the arm command, sleep for 'arm_delay' seconds instead of the regular delay
+        now = time.time()
+        if arm_delay - (now - start) > 0:
+            time.sleep(arm_delay - (now - start))
+        else:
+            print(f"{since(start)} - WARNING: loop took {now - start}s, longer than desired delay {arm_delay}s")
+        start = now
     else:
-        print(f"{since(start)} - WARNING: loop took {now - start}s, longer than desired delay {delay}s")
-    start = now
+        now = time.time()
+        if delay - (now - start) > 0:
+            time.sleep(delay - (now - start))
+        else:
+            print(f"{since(start)} - WARNING: loop took {now - start}s, longer than desired delay {delay}s")
+        start = now
 
+print(f"{since(t0)} - Done")
 # Close the serial port
 ser.close()
