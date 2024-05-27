3d8b0cfdddd742668f05f8032368927026e572b7
Petru Lupsac <4933508+shadow00@users.noreply.github.com>
2024-05-27 17:47:22 +0200

    Update formatlc.py to match new data format

--- test/formatlc.py
+++ test/formatlc.py
@@ -9,12 +9,14 @@ import scipy.stats as stats
 # Function to plot data from the third format of the input file (time series)
 def plot_data_load_cell(log_file):
     # Regular expression pattern to match lines with timestamp, load_cell
-    line_pattern = r'(?=\d{7,12}\,\d{1,4}\n)'
+    # 342394645,16414,342394995
+    line_pattern = r'(?=\d{7,12}\,\d{1,8}\,\d{7,12}\n)'
     # Regular expression pattern to extract timestamp, load_cell
-    extract_pattern = r'(\d{7,12})\,(\d{1,4})'
+    extract_pattern = r'(\d{7,12})\,(\d{1,8})\,(\d{7,12})'
 
     # Lists to store extracted values
-    timestamp = []
+    timestamp1 = []
+    timestamp2 = []
     load_cell = []
 
     # Read the log file line by line
@@ -25,10 +27,12 @@ def plot_data_load_cell(log_file):
                 # Extract timestamp1, v1, v2, and timestamp2 using regex
                 match = re.search(extract_pattern, line)
                 if match:
-                    timestamp.append(int(match.group(1)))
+                    timestamp1.append(int(match.group(1)))
                     load_cell.append(int(match.group(2)))
+                    timestamp2.append(int(match.group(3)))
 
-    timestamp = np.array(timestamp)
+    timestamp1 = np.array(timestamp1)
+    timestamp2 = np.array(timestamp2)
     load_cell = np.array(load_cell)
     # # Plot the data as time series, with some serious downsampling/averaging
 
@@ -41,18 +45,19 @@ def plot_data_load_cell(log_file):
     def moving_average(data, window_size):
         return np.convolve(data, np.ones(window_size) / window_size, mode='valid')
 
-    window_size = 2000  # Adjust this value based on the degree of smoothing desired
+    window_size = 100  # Adjust this value based on the degree of smoothing desired
     smoothed_load_cell = moving_average(load_cell, window_size)
 
     # Downsampling the data
-    downsample_factor = 2000
-    downsampled_timestamp = timestamp[:len(smoothed_load_cell):downsample_factor]
+    downsample_factor = 100
+    downsampled_timestamp = timestamp1[:len(smoothed_load_cell):downsample_factor]
     downsampled_load_cell = smoothed_load_cell[::downsample_factor]
 
     fig, ax = plt.subplots(figsize=(10, 6))
     # ax.scatter(timestamp, load_cell, marker='o', s=2, label='Data', alpha=0.5)
+    ax.plot(timestamp1, load_cell, label='Data', alpha=0.5)
     # ax.plot(timestamp[:len(smoothed_load_cell)], smoothed_load_cell, label=f'Smoothed (Window Size = {window_size})', linestyle='--')
-    ax.scatter(timestamp[:len(smoothed_load_cell)], smoothed_load_cell, label=f'Smoothed (Window Size = {window_size})', s=2)
+    # ax.scatter(timestamp[:len(smoothed_load_cell)], smoothed_load_cell, label=f'Smoothed (Window Size = {window_size})', s=2)
     ax.plot(downsampled_timestamp, downsampled_load_cell, label=f'Downsampled (Factor = {downsample_factor})', color="orange")
     # ax.plot(downsampled_timestamp, downsampled_load_cell, label=f'Downsampled (Factor = {downsample_factor})', color="blue")
     # ax.plot(xp, poly(xp), 'r-', label='Polynomial Regression')
@@ -61,6 +66,7 @@ def plot_data_load_cell(log_file):
     ax.set_xlabel('Time [s]')
     ax.set_ylabel('Load Cell')
     ax.set_title(f'Load cell over time for {log_file} (SMA = {window_size}, downsampling = {downsample_factor})')
+    # ax.set_title(f'Load cell over time for {log_file}')
     ax.grid(True)
     ax.legend()
     plt.show()
